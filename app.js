74f875d6abeab4062d6eb99e2bb37cb91df8b23e
igorcesar-dev <igorcesar.dev@gmail.com>
2023-03-05 13:24:03 -0300

    insert_cookie-parser

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const bodyParser = require("body-parser");
-var express = require("express");
-const flash = require("express-flash");
-const session = require("express-session");
-var app = express();
+let bodyParser = require("body-parser");
+let express = require("express");
+let flash = require("express-flash");
+let session = require("express-session");
+let app = express();
+let cookieParser = require("cookie-parser");
 
-app.set('view-engine', "ejs");
+app.set('view engine', 'ejs');
 
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -12,22 +13,44 @@ app.use(bodyParser.urlencoded({ extended: false }))
 //parse application/json
 app.use(bodyParser.json());
 
+app.use(cookieParser("nodejs"));
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true }
+    cookie: { maxAge: 60000 }
 }))
 
 app.use(flash());
 
+app.get("/", (req, res) => {
+    res.render("index")
+});
 
-app.get("/",(req,res) =>{
-    console.log("Está rodando....");
-    res.send("Rodando....")
-})
+app.post("/form", (req, res) => {
+    let { email, nome, pontos } = req.body;
+    let emailError;
+    let nomeError;
+    let pontosError;
 
+    if (email == "" || email == undefined) {
+        emailError = "Insira um email válido";
+    }
+    if (nome == "" || nome == undefined) {
+        nomeError = "Insira um nome válido";
+    }
 
-app.listen(5678,(req,res) => {
+    if (pontos < 20 || pontos == undefined) {
+        pontosError = "Insira pontos válidos";
+    }
+
+    if (emailError != undefined || pontosError != undefined || nomeError != undefined) {
+        res.redirect("/");
+    } else {
+        res.send("Sem erros de validação.");
+    }
+});
+
+app.listen(5678, (req, res) => {
     console.log("Servidor rodando!");
-})
+});
